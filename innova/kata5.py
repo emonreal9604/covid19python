ec10ca46a56c77ef2635be34d1f0d1f4b7b36f87
emonreal9604 <eleazarmonreal@outlook.com>
2022-02-12 22:54:23 -0600

    changes

--- innova/kata5.py
+++ innova/kata5.py
@@ -1,3 +1,17 @@
+# Crear variables para almacenar las dos distancias
+# ¡Asegúrate de quitar las comas!
+
+saturno = 1433500000
+neptuno = 778547200
+# Calcular la distancia entre planetas
+
+distancia_km = neptuno - saturno
+print(distancia_km)
+
+distance_millas = distancia_km * 1.609344
+print(distance_millas)
+##################################
+# Ejercicio 2
 # Almacenar las entradas del usuario
 first_planet = input(
     'Introduzca la distancia del sol para el primer planeta en KM: ')
